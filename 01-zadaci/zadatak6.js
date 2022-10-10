2fc75e7d230a2104e0ae8fe7a1ec1037ed5fdc0a
Anđela Matin <andjelamatin@gmail.com>
2022-10-10 21:47:39 +0200

    Rjesen Zadatak 6

---
+++ 01-zadaci/zadatak6.js
@@ -0,0 +1,8 @@
+var str = '“web apps vjezbe”';
+function pretvori(str) {
+    return str.replace(/(?:^\w|[A-Z]|\b\w)/g, function(word, index) {
+      return index === 0 ? word.toLowerCase() : word.toUpperCase();
+    }).replace(/\s+/g, '');
+  }
+
+console.log(pretvori(str));
